11d2b13f8f312c384ba0e7398c75914988609c85
newtonkiragu <karanunewton4@gmail.com>
2017-02-23 14:01:51 +0300

    finished product

--- js/scripts.js
+++ js/scripts.js
@@ -19,11 +19,66 @@ var modulo = function(number1, number2) {
 }
 
 $(document).ready(function() {
-    $("form#add").submit(function(event) {
-        event.preventDefault();
-        var number1 = parseFloat($("#add1").val());
-        var number2 = parseFloat($("#add2").val());
-        var result = add(number1, number2);
-        $("#output").text(result);
-    });
+  var result, number1, number2;
+
+          $("button#add").click(function () {
+            number1 = parseFloat($("#number1").val());
+            number2 = parseFloat($("#number2").val());
+            result = add(number1, number2);
+            $("#output").text(result);
+          });
+          $("button#subtract").click(function () {
+            number1 = parseFloat($("#number1").val());
+            number2 = parseFloat($("#number2").val());
+            result = subtract(number1, number2);
+            $("#output").text(result);
+          });
+          $("button#divide").click(function () {
+            number1 = parseFloat($("#number1").val());
+            number2 = parseFloat($("#number2").val());
+            result = divide(number1, number2);
+            $("#output").text(result);
+          });
+          $("button#multiply").click(function () {
+            number1 = parseFloat($("#number1").val());
+            number2 = parseFloat($("#number2").val());
+            result = multiply(number1, number2);
+            $("#output").text(result);
+          });
+          $("button#modulus").click(function () {
+            number1 = parseFloat($("#number1").val());
+            number2 = parseFloat($("#number2").val());
+            result = modulo(number1, number2);
+            $("#output").text(result);
+          });
+    // $("form#compute").submit(function(event) {
+    //     event.preventDefault();
+    //     var number1 = parseFloat($("#number1").val());
+    //     var number2 = parseFloat($("#number2").val());
+    //     var result;
+    //
+    //
+    //     // $("#output").text(result);
+    //     // var number1 = parseFloat($("#number1").val());
+    //     // var number2 = parseFloat($("#number2").val());
+    //     // var result = subtract(number1, number2);
+    //     // $("#output").text(result);
+    //     //
+    //     // var number1 = parseFloat($("#number1").val());
+    //     // var number2 = parseFloat($("#number2").val());
+    //     // var result = multiply(number1, number2);
+    //     // $("#output").text(result);
+    //     //
+    //     // var number1 = parseFloat($("#number1").val());
+    //     // var number2 = parseFloat($("#number2").val());
+    //     // var result = modulo(number1, number2);
+    //     // $("#output").text(result);
+    //     //
+    //     // var number1 = parseFloat($("#number1").val());
+    //     // var number2 = parseFloat($("#number2").val());
+    //     // var result = divide(number1, number2);
+    //     $("#output").text(result);
+    // });
+
+
 });
